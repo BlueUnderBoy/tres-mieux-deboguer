99063d5e328f6bbc16fa343ddb78b957704141ec
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-10-22 07:10:00 +0000

    plus de les corriges

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.new
-    comment.author_id = params.fetch("query_author_id")
-    comment.venue_id = params.fetch("query_venue_id")
-    comment.body = params.fetch("query_body")
+    comment.author_id = params.fetch("author_id")
+    comment.venue_id = params.fetch("venue_id")
+    comment.body = params.fetch("body")
     comment.save
     
     redirect_to("/venues/#{comment.venue_id}")
